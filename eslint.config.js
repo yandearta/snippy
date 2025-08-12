52ef5f0cd9a0cd498e0a4002ee0c5efbf3b5c234
Yande Arta <yandearta00@gmail.com>
2025-08-12 23:06:38 +0800

    chore: init

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+import antfu from '@antfu/eslint-config'
+
+import js from '@eslint/js'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import { globalIgnores } from 'eslint/config'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+export default antfu(
+  {
+    formatters: true,
+  },
+  tseslint.config([
+    globalIgnores(['dist']),
+    {
+      files: ['**/*.{ts,tsx}'],
+      extends: [
+        js.configs.recommended,
+        tseslint.configs.recommended,
+        reactHooks.configs['recommended-latest'],
+        reactRefresh.configs.vite,
+      ],
+      languageOptions: {
+        ecmaVersion: 2020,
+        globals: globals.browser,
+      },
+    },
+  ]),
+)
